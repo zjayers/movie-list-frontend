03548f0d00d7178917d12bd1f8121ff9f9e54103
Zach Ayers <ayers.dev@icloud.com>
2020-05-29 00:21:37 -0700

    Front End Complete - Tied To Backend Server

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const fetchMovies = async (searchTerm) => {
-  const response = await axios.get('http://127.0.0.1:3000/api/v1/');
+  const response = await axios.get(
+    'https://movie-list-ayers.herokuapp.com/api/v1/',
+  );
   // If there is no data on the response, return an empty array
   if (!response.data.data) {
     return [];
